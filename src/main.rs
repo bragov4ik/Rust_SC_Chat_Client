03023c0c905c60e8e1c96fd317e6f58b377f9c2c
PurplePegasuss <k.glinsky@innopolis.university>
2021-04-16 20:54:25 +0300

    Added authentication system, fixed minor errors, added code description and code clean up.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::{Read, Write, stdin};
 use std::net::TcpStream;
 
-// Reads from the stream until it recieves '\r\n\r\n' sequence
+// Reads from the stream until it receives '\r\n\r\n' sequence
 fn read_until_2rn(stream: &mut TlsStream<TcpStream>, buf: &mut Vec<u8>) {
     let mut inner_buf = [0];
     let mut was_r = false;
@@ -27,42 +27,52 @@ fn read_until_2rn(stream: &mut TlsStream<TcpStream>, buf: &mut Vec<u8>) {
             true
         },
         Err(e) => {
-            println!("Error while recieving message: {}", e);
+            println!("Error while receiving message: {}", e);
             false
         }
     } {}
 }
 
 fn main() {
+    // Tool to establish TLS connections
     let mut connector = TlsConnector::builder();
 
+    // Unfolding and usage of certificate for a user
     let mut file = File::open("certificate.crt").unwrap();
     let mut certificate = vec![];
     file.read_to_end(&mut certificate).unwrap();
     let certificate = Certificate::from_pem(&certificate).unwrap();
-
     connector.add_root_certificate(certificate);
-    let connector = connector.build().unwrap();
 
+    let connector = connector.build().unwrap();
     let stream = TcpStream::connect("localhost:8080").unwrap();
     let mut stream = connector.connect("localhost", stream).unwrap();
 
-    // Send message
-    println!("Sending '{}'", "GET / HTTP/1.0\r\n\r\n");
-    stream.write_all(b"GET / HTTP/1.0\r\n\r\n").unwrap();
+    // Value to stop iterating through a loop then we receive correct credentials
+    let mut authentication_incorrect: bool = true;
+    // Do not stop, until the credentials are correct
+    while authentication_incorrect {
+        println!("Log in format: login/password");
+        println!("Registration format: login/password/username");
+        // Providing Credentials for Server
+        let mut login_buf = String::new();
+        stdin().read_line(&mut login_buf).unwrap();
+        login_buf += "\r\n\r\n";
+        stream.write_all(login_buf.as_bytes()).unwrap();
 
+        // Receive response from the server
+        let mut res = vec![];
+        read_until_2rn(&mut stream, &mut res);
+        let ind_cred_res = String::from_utf8_lossy(&res);
 
-    // Recieve response
-    let mut res = vec![];
-    read_until_2rn(&mut stream, &mut res);
-    println!("Recieved '{}'", String::from_utf8_lossy(&res));
-
-    // Send another message from user input
-    let mut buf = String::new();
-    stdin().read_line(&mut buf).unwrap();
-    buf += "\r\n\r\n";
-    println!("Sending '{}'", buf);
-    stream.write_all(buf.as_bytes()).unwrap();
+        // If the response says our credentials are correct, we stop iterating
+        if ind_cred_res == "correct\r\n\r\n" {
+            authentication_incorrect = false
+        } else {
+            // If the response says our credentials are incorrect, we continue iterating
+            println!("{}",ind_cred_res.trim_end_matches("\r\n\r\n"));
+        }
+    }
 
     // Close connection
     stream.shutdown().unwrap();
